e29dd4037073b3694661be2f6178e3183b4df68f
justinfmorgan <justinfmorgan@gmail.com>
2019-04-23 21:29:04 -0400

    Added EnvUnsafe and some updates to Check

--- project/src/Check.hs
+++ project/src/Check.hs
@@ -13,12 +13,24 @@ import Ast
 --   * use of undefined variable
 --   * defined but unused variable
 --   * type errors
-data WarningMsg = 
-    UndefinedVarUse String  -- ^ This is the Warning for use of Undefined variable name
+data WarningMsg = UndefinedVarUse String  -- ^ This is the Warning for use of Undefined variable name
+                | UnusedVar String
+                | TypeError String
+
   -- ...
   deriving (Show,Eq)
 
 -- | perform static checking on the Ast
 -- the output a set of warning on that input Ast
 check :: Ast -> Set WarningMsg
-check = undefined
+check t = ()
+
+-- collect all the vars that appear in the expression that are not bound
+freeVars :: Ast -> Set String
+freeVars (Var s) = Set.singleton s
+freeVars (App t1 t2) = Set.union (freeVars t1) (freeVars t2)
+freeVars (Lam s t1) = Set.delete s (freeVars t1)
+
+-- when are there no free variables in a lambda expression?
+isClosed :: Ast -> Bool
+isClosed t = ((freeVars t) == Set.empty)
---
+++ project/src/EnvUnsafe.hs
@@ -0,0 +1,50 @@
+module EnvUnsafe where
+
+import Control.Monad(ap)
+
+--This monad will form the plumbing for the evaluation function
+
+data Unsafe a = Error String | Ok a deriving (Show, Eq)
+
+data EnvUnsafe e a = EnvUnsafe (e -> Unsafe a )
+
+-- function that just runs the function contained in EnvUnsafe
+runEnvUnsafe ::  (EnvUnsafe e a) -> e -> Unsafe a
+runEnvUnsafe (EnvUnsafe eu) e = eu e
+
+-- a way to easily return an error (for instance in do notation)
+err :: String -> EnvUnsafe e a
+err s = EnvUnsafe $ \ _ -> Error s
+
+
+-- a way to easily get the entire environment (for instance in do notation)
+getEnv :: EnvUnsafe e e
+getEnv = EnvUnsafe $ \ env -> Ok env
+
+
+instance Functor (EnvUnsafe e) where
+  -- fmap :: (a -> b) -> EnvUnsafe a -> EnvUnsafe b
+  fmap f (EnvUnsafe eu) = EnvUnsafe $ \ e -> 
+    case eu e of
+      Ok a -> Ok $ f a
+      Error s -> Error s
+  -- make sure your implementation follows the functor laws
+
+--ignore this for now
+instance Applicative (EnvUnsafe e) where
+  pure = return
+  (<*>) = ap
+
+instance Monad (EnvUnsafe e) where
+  --return :: a -> EnvUnsafe a
+  return a = EnvUnsafe $ \ e -> Ok a
+
+  --(>>=) :: EnvUnsafe a -> (a -> EnvUnsafe b) -> EnvUnsafe b
+  (EnvUnsafe eu) >>= f = EnvUnsafe $ \ e ->
+    case eu e of
+      Error s -> Error s
+      Ok a -> runEnvUnsafe (f a) e
+
+  -- make sure your implementation follows the Monad laws
+
+-- technical note: this could be done more succinctly with monad transformers, but it is also good practice to do it by hand
