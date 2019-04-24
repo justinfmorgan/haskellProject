3dc5417fd4df216e8d2ebb3efdb472d6b327da1b
Noah Abdulkarim Malhi <malhin@bu.edu>
2019-04-23 21:23:28 -0400

    Added part of show pretty

--- project/src/Ast.hs
+++ project/src/Ast.hs
@@ -54,4 +54,25 @@ showFullyParen = undefined
 showPretty :: Ast  -- ^ The Ast to show
             -> Integer  -- ^ The precedence of the root expression, see the doc for 'HelpShow.parenthesize' for more detail
             -> String  -- ^ the minimally parenthesized string representing the input Ast
-showPretty = undefined
+showPretty (ValInt i) _ =  if i < 0
+                           then  "(" ++ show i ++ ")"
+                           else show i
+showPretty (ValBool True) _ =  "true"
+showPretty (ValBool False)  _  = "false"
+showPretty Nil _ = "[]"
+showPretty (Var s) _ = s
+
+showPretty (Lam v bod) i = parenthesize 1 i $ "\\ " ++ v ++ " -> " ++ (showPretty bod 1)
+showPretty (Let v a bod)  i = parenthesize 1 i $  "let " ++ v ++ " = " ++ (showPretty a 1) ++ " in " ++ (showPretty bod 1)
+showPretty (If b t e) i = parenthesize 1 i $  "if " ++ (showPretty b 1) ++ " then " ++ (showPretty t 1) ++ " else " ++ (showPretty e 1)
+
+showPretty (App l r) i = parenthesize 2 i $ (showPretty l 2) ++ " " ++ (showPretty r 3)
+showPretty (Cons l r) i = parenthesize 4 i $ (showPretty l 5) ++ " : " ++ (showPretty r 4)
+showPretty (Or l r) i = parenthesize 6 i $ (showPretty l 6) ++ " || " ++ (showPretty r 7)
+showPretty (And l r) i = parenthesize 8 i $ (showPretty l 8) ++ " && " ++ (showPretty r 9)
+showPretty (Minus l r) i = parenthesize 10 i $ (showPretty l 10) ++ " - " ++ (showPretty r 11)
+showPretty (Plus l r) i = parenthesize 10 i $ (showPretty l 10) ++ " + " ++ (showPretty r 11)
+showPretty (Mult l r) i = parenthesize 12 i $ (showPretty l 12) ++ " * " ++ (showPretty r 13)
+showPretty (Div l r) i = parenthesize 12 i $ (showPretty l 12) ++ " / " ++ (showPretty r 13)
+
+showPretty (Not l ) i = parenthesize 14 i $  " ! " ++ (showPretty l 14)
