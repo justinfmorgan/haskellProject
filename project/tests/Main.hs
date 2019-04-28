598400190fa1e120690b86b2b6acc853f8f7c432
justinfmorgan <justinfmorgan@gmail.com>
2019-04-28 19:26:08 -0400

    Added some tests to evaltest and parsertest

--- project/tests/Main.hs
+++ project/tests/Main.hs
@@ -6,19 +6,18 @@ import System.Environment
 import CheckTest
 import EvalTest
 import ParserTest
---main = defaultMain testSuite
 main = 
     do 
         setEnv "TASTY_TIMEOUT" "40s"
         setEnv "TASTY_QUICKCHECK_TESTS" "1000" --TODO: I never trust less than 10000
         setEnv "TASTY_QUICKCHECK_MAX_SIZE" "50"
-        defaultMain allTests
+        defaultMain testSuite
         unsetEnv "TASTY_TIMEOUT"
         unsetEnv "TASTY_QUICKCHECK_TESTS"
         unsetEnv "TASTY_QUICKCHECK_MAX_SIZE"
 
 
-allTests =
+testSuite =
   testGroup
     "allTests"
     [
