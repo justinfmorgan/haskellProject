62f116f202a6d91c10fb27a93534d6cd9ca157ba
justinfmorgan <justinfmorgan@gmail.com>
2019-05-02 16:09:41 -0400

    Alex is a big butthead

--- project/src/Eval.hs
+++ project/src/Eval.hs
@@ -284,7 +284,7 @@ eval (If a b c) = do a' <- (eval a)
                           F x -> if (x > 0) then (eval b) else (eval c)
                           _   -> err "if requires a bool, int or float!"
 eval (Let v val bod) = eval (App (Lam v bod) val)
-eval (Letrec v val bod) = undefined --TODO
+eval (Letrec v val bod) = eval (App (Lam "f" (Lam "x" ((Var "x") (Var "x")))) (Lam "x2" (App ())))
 eval (DotMixIn f g) = eval (Lam "x" (App f (App g (Var "x"))))
 eval (Lam x bod) = do env <- getEnv
                       return $ Fun $ \ v -> runEnvUnsafeLog (eval bod) (Map.insert x v env)
